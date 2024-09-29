73e12b8f54360709b6942d51cfe23ea3c4a3721d
chrisaharden <68924870+chrisaharden@users.noreply.github.com>
2024-09-29 15:38:30 -0400

    updated nightly command file to output all text to a log file
    Also added a sample log file, so you know what to expect

--- cleanmailnightly.cmd
+++ cleanmailnightly.cmd
@@ -1,3 +1,5 @@
+REM @echo off
 REM Copy the config.json file, add the specific model you want, and process nightly like this.
-python main.py config-3.0-Haiku.json
-
+REM Redirect all console output to ./output/log.txt
+if not exist ".\output" mkdir ".\output"
+python main.py config-3.0-Haiku.json > ".\output\log.txt" 2>&1
